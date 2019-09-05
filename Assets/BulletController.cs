06907e2db7706ee7d2e7529cc4ee50eb6a99d249
Yoshitake Kitamura <kitamura@epea.co.jp>
2019-09-05 16:33:33 +0900

    衝突判定まで

---
+++ Assets/BulletController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (0, 0.2f, 0);
+
+		if (transform.position.y > 5) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		Destroy (coll.gameObject);
+		Destroy (gameObject);
+	}
+}
